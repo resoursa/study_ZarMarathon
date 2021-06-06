38470e9d347f2dae96a3fc75abdd18507a13c7f9
resoursa <resours@gmail.com>
2021-06-06 10:29:28 +0300

    homework-3

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+const arena = document.querySelector('.arenas');
+const randomButton = document.querySelector('.button');
+
 
 // Object Player 1
 const player1 = {
     id: 1,
     name: 'Scorpion',
-    hp: 70,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: [
         'weapon1',
@@ -12,7 +15,7 @@ const player1 = {
         'weapon4'
     ],
     attack: function() {
-        console.log(player1.name + 'fight');
+        console.log(this.name + 'fight');
     }
 }
 
@@ -29,48 +32,89 @@ const player2 = {
         'weapon4'
     ],
     attack: function() {
-        console.log(player2.name + 'fight');
+        console.log(this.name + 'fight');
     }
 }
 
 
-const arena = document.querySelector('.arenas');
+// Create DOM ELement
+function createDomEl(tag, className) {
+    const tagEl = document.createElement(tag);
+
+    className ? tagEl.classList.add(className) : ''
+
+    return tagEl;
+}
+
+// Math 1 - 20 random
+
+function mathRandom() {
+    let result = Math.ceil(Math.random() * 20);
+
+    return result;
+}
 
 // Create player Function
-function createPlayer(playerId, playerObj) {
+function createPlayer(playerObj) {
+    
     // Main div for first player
-    const player = document.createElement('div');
-    player.classList.add(playerId); // player id parametr string
-
+    const player = createDomEl('div', 'player' + playerObj.id);
+    
     // Progressbar div with life and name
-    const progressbar = document.createElement('div');
-    progressbar.classList.add('progressbar');
+    const progressbar = createDomEl('div', 'progressbar');
 
-    const life = document.createElement('div');
-    life.classList.add('life');
+    const life = createDomEl('div', 'life');
     life.style.width = playerObj.hp + '%'; // player object hp num
 
-    const name = document.createElement('div');
-    name.classList.add('name');
+    const name = createDomEl('div', 'name');
     name.innerText = playerObj.name; // player object name string
 
     // Caracter with image div
-    const character = document.createElement('div');
-    character.classList.add('character');
+    const character = createDomEl('div', 'character');
 
-    const caracterImage = document.createElement('img');
+    const caracterImage = createDomEl('img');
     caracterImage.src = playerObj.img; // player object image string
 
 
     // Append nodes
-    arena.appendChild(player);
     character.appendChild(caracterImage);
     player.append(progressbar, character);
     progressbar.append(life, name);
+
+    return player;
 }
 
-createPlayer('player1', player1);
-createPlayer('player2', player2);
+// Change HP function
+function changeHP(playerObj) {
+    const playerLife = document.querySelector('.player' + playerObj.id + ' .life');
+    playerObj.hp -= mathRandom();
+    console.log(playerLife);
+    playerLife.style.width = playerObj.hp >= 0 ? playerObj.hp + '%' : 0;;
+
+    // Here might be better :(
+    if (player1.hp <= 0) {
+        arena.appendChild(playerWin(player2.name));
+        randomButton.disabled = true;
+    }
+    if (player2.hp <= 0) {
+        arena.appendChild(playerWin(player1.name));
+        randomButton.disabled = true;
+    }
+}
+
+// Player Lose Alert
+function playerWin(name) {
+    const loseTitle = createDomEl('div', 'loseTitle');
+    loseTitle.innerText = name + ' wins';
+
+    return loseTitle;
+}
 
 
-    
+randomButton.addEventListener('click', function() {
+    changeHP(player1);
+    changeHP(player2);
+})
+
+arena.appendChild(createPlayer(player1));
+arena.appendChild(createPlayer(player2));    
