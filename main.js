0bc436a9b58d7da9501318659c8edaf56c324d88
resoursa <resours@gmail.com>
2021-06-11 12:28:11 +0300

    homework-6 before start

--- main.js
+++ main.js
@@ -10,6 +10,46 @@ const HIT = {
 }
 const ATTACK = ['head', 'body', 'foot'];
 
+const logs = {
+    start: 'Часы показывали [time], когда [player1] и [player2] бросили вызов друг другу.',
+    end: [
+        'Результат удара [playerWins]: [playerLose] - труп',
+        '[playerLose] погиб от удара бойца [playerWins]',
+        'Результат боя: [playerLose] - жертва, [playerWins] - убийца',
+    ],
+    hit: [
+        '[playerDefence] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.',
+        '[playerDefence] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.',
+        '[playerDefence] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.',
+        '[playerDefence] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.',
+        '[playerDefence] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.',
+        '[playerDefence] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.',
+        '[playerDefence] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.',
+        '[playerDefence] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.',
+        '[playerDefence] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.',
+        '[playerDefence] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.',
+        '[playerDefence] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.',
+        '[playerDefence] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.',
+        '[playerDefence] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.',
+        '[playerDefence] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.',
+        '[playerDefence] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.',
+        '[playerDefence] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.',
+        '[playerDefence] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.',
+        '[playerDefence] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.',
+    ],
+    defence: [
+        '[playerKick] потерял момент и храбрый [playerDefence] отпрыгнул от удара открытой ладонью в ключицу.',
+        '[playerKick] не контролировал ситуацию, и потому [playerDefence] поставил блок на удар пяткой в правую грудь.',
+        '[playerKick] потерял момент и [playerDefence] поставил блок на удар коленом по селезенке.',
+        '[playerKick] поскользнулся и задумчивый [playerDefence] поставил блок на тычок головой в бровь.',
+        '[playerKick] старался провести удар, но непобедимый [playerDefence] ушел в сторону от удара копчиком прямо в пятку.',
+        '[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.',
+        '[playerKick] не думал о бое, потому расстроенный [playerDefence] отпрыгнул от удара кулаком куда обычно не бьют.',
+        '[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.'
+    ],
+    draw: 'Ничья - это тоже победа!'
+};
+
 // Object Player 1
 const player1 = {
     id: 1,
@@ -130,6 +170,7 @@ function resultText(name) {
     return resultOutput;
 }
 
+// Reload btn for restarn
 function createReloadButton() {
     const wrapper = createDomEl('div', 'reloadWrap');
     const reloadBtn = createDomEl('button', 'button');
@@ -147,7 +188,7 @@ function createReloadButton() {
     })
 }
 
-// Attack Function
+// Enemy Function
 function enemyAttack() {
     // Math random based on array quantity
     const hit = ATTACK[mathRandom(ATTACK.length) - 1]
@@ -160,12 +201,11 @@ function enemyAttack() {
     }
 }
 
-formEl.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const enemy = enemyAttack();
+// Player Attack Function
+function playerAttack() {
     const attack = {};
 
+    // Looking for checked radio
     for(let item of formEl) {
         if(item.checked && item.name === 'hit') {
             attack.value = mathRandom(HIT[item.value]);
@@ -179,22 +219,10 @@ formEl.addEventListener('submit', function(e) {
         item.checked = false;
     }
 
-    console.log(attack, enemy);
+    return attack;
+}
 
-    if(enemy.hit !== attack.defence) {
-        player1.changeHp(enemy.value);
-        player1.renderHP();
-    }
-    if(attack.hit !== enemy.defence) {
-        player2.changeHp(attack.value);
-        player2.renderHP();
-    }
-
-    if (player1.hp === 0 || player2.hp === 0) {
-        formEl.remove();
-        createReloadButton();
-    }
-
+function showResult() {
     // Lose/Win/Draw Check
     if (player1.hp <= 0 && player1.hp < player2.hp) {
         arena.appendChild(resultText(player2.name));
@@ -203,6 +231,35 @@ formEl.addEventListener('submit', function(e) {
     } else if (player1.hp === 0 && player2.hp === 0) {
         arena.appendChild(resultText());
     }
+}
+
+formEl.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Filling the objects fields
+    const enemy = enemyAttack();
+    const player = playerAttack();
+
+    console.log(player, enemy); // Just incase
+
+    // Check if hit blocked
+    if(enemy.hit !== player.defence) {
+        player1.changeHp(enemy.value);
+        player1.renderHP();
+    }
+    if(player.hit !== enemy.defence) {
+        player2.changeHp(player.value);
+        player2.renderHP();
+    }
+
+    // Remove form after finish, and add reload btn
+    if (player1.hp === 0 || player2.hp === 0) {
+        formEl.remove();
+        createReloadButton();
+    }
+
+    // Lose/Win/Draw
+    showResult();
 
 })
 
